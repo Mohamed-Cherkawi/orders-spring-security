3823185968bd28ac0dc93f9800e85d8994c7bc9a
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2022-12-23 15:08:06 +0100

    removed form and basic auth config and add some for the jwt and add the daoAuthenticationProvider bean that will verify the validity of users credentials

--- src/main/java/com/example/demo/config/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/config/ApplicationSecurityConfig.java
@@ -1,24 +1,59 @@
 package com.example.demo.config;
 
+import com.example.demo.config.jwt.JwtConfig;
+import com.example.demo.config.jwt.JwtTokenVerifier;
+import com.example.demo.config.jwt.JwtUsernameAndPasswordAuthenticationFilter;
+import com.example.demo.service.ApplicationUserService;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-//@Configuration
+import javax.crypto.SecretKey;
+
+@Configuration
 @EnableWebSecurity
-public class ApplicationSecurityConfig {
+@AllArgsConstructor
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final PasswordEncoder passwordEncoder;
+    private final ApplicationUserService applicationUserService;
+    private final JwtConfig jwtConfig;
+    private final SecretKey secretKey;
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
+                .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS) //method is used to configure the session management policy for the application which specifies that the application should not create any server-side sessions.
+                .and()
+                .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager(),jwtConfig,secretKey)) // method is used to add custom filters to the filter chain for an application and this filter will apply for each request and response
+                .addFilterAfter(new JwtTokenVerifier(secretKey,jwtConfig), JwtUsernameAndPasswordAuthenticationFilter.class)
                 .authorizeRequests()
                 .anyRequest()
-                .authenticated()
-                .and().formLogin()
-                .and().httpBasic();
+                .authenticated();
 
-        return http.build();
     }
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(daoAuthenticationProvider());
+    }
+
+    //The AuthenticationProvider interface is a strategy interface used by the Spring Security framework to authenticate users. An AuthenticationProvider implementation is responsible for verifying the authenticity of a user's credentials and returning an Authentication object if the credentials are valid.
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder);
+        provider.setUserDetailsService(applicationUserService);
+        return provider;
+    }
 }
