e209889944265ee959a2da551b50e68cd5f0878a
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2022-12-23 23:41:42 +0100

    This class contains the method that will process & verify and validate the token given by the user

---
+++ src/main/java/com/example/demo/config/jwt/JwtTokenVerifier.java
@@ -0,0 +1,70 @@
+package com.example.demo.config.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import lombok.AllArgsConstructor;
+import org.springframework.lang.NonNullApi;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.crypto.SecretKey;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
+public class JwtTokenVerifier extends OncePerRequestFilter {
+    private final SecretKey secretKey;
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        String authorizationHeader = request.getHeader(JwtConfig.getAuthorizationHeader());
+        if(authorizationHeader == null || !authorizationHeader.startsWith(JwtConfig.TOKEN_PREFIX)){
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        String token = authorizationHeader.replace(JwtConfig.TOKEN_PREFIX,"");
+        try {
+
+            Jws<Claims> claimsJws = Jwts.parser()
+                    .setSigningKey(secretKey)
+                    .parseClaimsJws(token);
+
+            Claims body = claimsJws.getBody();
+
+            String username = body.getSubject();
+
+            var authorities = (List<Map<String,String>>) body.get("authorities");
+
+            Set<SimpleGrantedAuthority> simpleGrantedAuthoritySet = authorities.stream()
+                    .map(item -> new SimpleGrantedAuthority(item.get("authority")))
+                    .collect(Collectors.toSet());
+
+            Authentication authentication = new UsernamePasswordAuthenticationToken(
+                    username,
+                    null,
+                    simpleGrantedAuthoritySet
+            );
+
+            SecurityContextHolder.getContext().setAuthentication(authentication); // This method is used to set the current authentication object in the security context.
+        } catch (JwtException e){
+            throw new IllegalStateException(String.format("Token %s cannot be trusted",token));
+        }
+
+        filterChain.doFilter(request,response); // Sending back the response to other filter in our FIlterChain process
+    }
+}
