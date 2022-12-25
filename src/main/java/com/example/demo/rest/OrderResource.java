15570090c79133f58ec855cfdff78bbfa14998d0
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2022-12-25 21:26:20 +0100

    Renamed the OrderService Class and added an api for getting all the orders made by the client

--- src/main/java/com/example/demo/rest/OrderResource.java
+++ src/main/java/com/example/demo/rest/OrderResource.java
@@ -1,30 +1,29 @@
 package com.example.demo.rest;
 
-
-
 import com.example.demo.domain.Order;
 import com.example.demo.service.OrderService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestBody;
 
-
-
 import javax.validation.Valid;
+import java.util.List;
 
-@RestController
+@RestController @AllArgsConstructor
 @RequestMapping("/api/v1/orders")
-public class CommandeResource {
+public class OrderResource {
 
-    @Autowired
     OrderService orderService;
 
-    @PostMapping("/") @ResponseBody
+    @GetMapping
+    public List<Order> showAllOrders(){
+       return orderService.getAllOrders();
+    }
+    @PostMapping("/create-new-order") @ResponseBody
     public Order save(@RequestBody @Valid Order order) {
         return orderService.save(order);
     }
