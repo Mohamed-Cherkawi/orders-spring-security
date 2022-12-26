d3e8f9236bb6485f095c4b0a9ca276c7f8840008
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2022-12-26 17:11:04 +0100

    Refactored and removed useless code

--- src/main/java/com/example/demo/repository/ApplicationUserDaoImp.java
+++ src/main/java/com/example/demo/repository/ApplicationUserDaoImp.java
@@ -2,30 +2,24 @@ package com.example.demo.repository;
 
 import com.example.demo.domain.UserApp;
 import com.example.demo.service.interfaces.ApplicationUserDao;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-@Repository("user-repo")
+@Repository("user-repo") @AllArgsConstructor
 public class ApplicationUserDaoImp implements ApplicationUserDao {
     private final UserRepository userRepository;
 
-    @Autowired
-    public ApplicationUserDaoImp(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
     @Override
     public Optional<UserApp> selectApplicationUserByUsername(String username) {
+
         return getApplicationUsers()
                 .stream()
                 .filter(user -> username.equals(user.getUsername()))
                 .findFirst();
     }
 
-    private List<UserApp> getApplicationUsers() {
-        return userRepository.findAll() ;
-    }
+    private List<UserApp> getApplicationUsers() { return userRepository.findAll() ;}
 }
